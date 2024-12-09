778edc04044c597c514774c6f2e880f43f26e90d
Shreyas M <shreyas.m246418@gmail.com>
2024-12-09 16:45:20 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="page-container">
+      <h1>About Us</h1>
+      <p>We are dedicated to connecting talented professionals with great companies.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div className="page-container">
+      <h1>Contact Us</h1>
+      <p>Get in touch with our team for any inquiries.</p>
+      <div className="contact-info">
+        <p>Email: contact@jobhub.com</p>
+        <p>Phone: (555) 123-4567</p>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
